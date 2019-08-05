b67ed07e56b44518acdb8ca63614224662a87139
dislersd <dislersd@gmail.com>
2019-08-05 06:37:40 -0700

    updated index to use yoga - updated user table

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
 
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer } from "graphql-yoga";
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
 const resolvers = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
+    hello: (_: any, { name }: any) => `Jello ${name || "World"}`
+  }
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
