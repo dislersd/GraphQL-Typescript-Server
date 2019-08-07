0e1b547d0552bf5a2c876a9bdbd966c7e768b566
dislersd <dislersd@gmail.com>
2019-08-06 22:58:21 -0700

    ✅ testing query response

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,7 +1,9 @@
 import { request } from "graphql-request";
 import { host } from "./constants";
+import { User } from "../entity/User";
+import { createConnections } from "typeorm";
 
-const email = "bob@bob.com";
+const email = "tom@bob.com";
 const password = "password";
 
 const mutation = `
@@ -13,4 +15,14 @@ mutation {
 test("Register User", async () => {
   const response = await request(host, mutation);
   expect(response).toEqual({ register: true });
+  await createConnections();
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
 });
+
+// TODO use a test database
+// drop all data once the test is over
+// when I run yarn test it also starts the server
