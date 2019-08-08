95174529d7d32e3fc7662379e9b8fe5476dbdda1
dislersd <dislersd@gmail.com>
2019-08-08 16:02:04 -0700

    🙌 seperating schemas/resolvers into modules

--- src/startserver.ts
+++ src/startserver.ts
@@ -1,19 +1,30 @@
 import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
 import * as path from "path";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { AddressInfo } from "net";
 
-import { resolvers } from "./resolvers";
 import { createTypeOrmConn } from "./utils/createTypeOrmConn";
+import { GraphQLSchema } from "graphql";
 
 export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
 
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createTypeOrmConn();
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000
   });
-  const port = app.address();
+  const { port } = app.address() as AddressInfo;
   console.log(`Server is running at localhost:${port}`);
 
   return app;
