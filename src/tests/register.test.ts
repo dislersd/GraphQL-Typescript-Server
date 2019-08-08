747ca29bade561aa12b8a34ad2458ad2f220a634
dislersd <dislersd@gmail.com>
2019-08-07 19:00:05 -0700

    ✨ created test database - added dynamic typeorm connection

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,9 +1,13 @@
 import { request } from "graphql-request";
 import { host } from "./constants";
 import { User } from "../entity/User";
-import { createConnections } from "typeorm";
+import { createTypeOrmConn } from "../utils/createTypeOrmConn";
 
-const email = "tom@bob.com";
+beforeAll(async () => {
+  await createTypeOrmConn();
+});
+
+const email = "bob@bob.com";
 const password = "password";
 
 const mutation = `
@@ -15,14 +19,15 @@ mutation {
 test("Register User", async () => {
   const response = await request(host, mutation);
   expect(response).toEqual({ register: true });
-  await createConnections();
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
+  // password should be hashed
   expect(user.password).not.toEqual(password);
 });
 
-// TODO use a test database
+// TODO
+// use a test database
 // drop all data once the test is over
 // when I run yarn test it also starts the server
