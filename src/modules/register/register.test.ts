3f2e14ac786194aecd8a5cc5998219eef1c105b8
dislersd <dislersd@gmail.com>
2019-08-09 22:37:21 -0700

    ✅ handling duplicate user creation - test pass

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -8,9 +8,7 @@ let getHost = () => "";
 
 beforeAll(async () => {
   const app = await startServer();
-
   const { port } = app.address() as AddressInfo;
-  console.log(port);
   getHost = () => `http://127.0.0.1:${port}`;
 });
 
@@ -19,17 +17,23 @@ const password = "password";
 
 const mutation = `
 mutation {
-    register(email: "${email}", password: "${password}")
+    register(email: "${email}", password: "${password}") {
+        path
+        message
+    }
 }
 `;
 
 test("Register User", async () => {
   const response = await request(getHost(), mutation);
-  expect(response).toEqual({ register: true });
+  expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   // password should be hashed
   expect(user.password).not.toEqual(password);
+  const response2: any = await request(getHost(), mutation);
+  expect(response2.register).toHaveLength(1);
+  expect(response2.register[0].path).toEqual("email");
 });
