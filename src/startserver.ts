38a08dcf0936bd7cfac4c24d39b3c8514a0d7500
dislersd <dislersd@gmail.com>
2019-08-11 09:03:27 -0700

    💥 setting up redis for emailconfirm

--- src/startserver.ts
+++ src/startserver.ts
@@ -4,9 +4,11 @@ import * as path from "path";
 import * as fs from "fs";
 import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
 import { AddressInfo } from "net";
+import { GraphQLSchema } from "graphql";
+import * as Redis from "ioredis";
 
 import { createTypeOrmConn } from "./utils/createTypeOrmConn";
-import { GraphQLSchema } from "graphql";
+import { User } from "./entity/User";
 
 export const startServer = async () => {
   const schemas: GraphQLSchema[] = [];
@@ -19,7 +21,27 @@ export const startServer = async () => {
     schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
   });
 
-  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
+  const redis = new Redis();
+
+  const server = new GraphQLServer({
+    schema: mergeSchemas({ schemas }),
+    context: ({ request }) => ({
+      redis,
+      url: request.protocol + "://" + request.get("host")
+    })
+  });
+
+  server.express.get("/confirm/:id", async (req, res) => {
+    const { id } = req.params;
+    const userId = await redis.get(id);
+    if (userId) {
+      await User.update({ id: userId }, { confirmed: true });
+      res.send("ok");
+    } else {
+      res.send("invalid");
+    }
+  });
+
   await createTypeOrmConn();
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000
