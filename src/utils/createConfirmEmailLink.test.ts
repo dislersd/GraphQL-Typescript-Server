9272889f8a7410b8a2d9ab109cdae437ba516d8d
dislersd <dislersd@gmail.com>
2019-08-12 10:32:59 -0700

    🌵 separating confirm email function to own folder

--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -1,12 +1,11 @@
 import { createConfirmEmailLink } from "./createConfirmEmailLink";
 import { createTypeOrmConn } from "./createTypeOrmConn";
 import { User } from "../entity/User";
-import * as Redis from "ioredis";
 import fetch from "node-fetch";
 import { Connection } from "typeorm";
+import { redis } from "../redis";
 
 let userId = "";
-let redis = new Redis();
 
 let conn: Connection;
 
@@ -20,31 +19,23 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-  conn.close();
+  await conn.close();
 });
 
-describe("test createConfirmEmailLink works", () => {
-  test("confirms user and clears key in redis", async () => {
-    const url = await createConfirmEmailLink(
-      process.env.TEST_HOST as string,
-      userId,
-      redis,
-    );
+test("confirms user and clears key in redis", async () => {
+  const url = await createConfirmEmailLink(
+    process.env.TEST_HOST as string,
+    userId,
+    redis
+  );
 
-    const response = await fetch(url);
-    const text = await response.text();
-    expect(text).toEqual("ok");
-    const user = await User.findOne({ where: { id: userId } });
-    expect((user as User).confirmed).toBeTruthy();
-    const chunks = url.split("/");
-    const key = chunks[chunks.length - 1];
-    const value = await redis.get(key);
-    expect(value).toBeNull();
-  });
-
-  test("sends invalid back if bad id sent", async () => {
-    const response = await fetch(`${process.env.TEST_HOST}/confirm/1295`);
-    const text = await response.text();
-    expect(text).toEqual("invalid");
-  });
+  const response = await fetch(url);
+  const text = await response.text();
+  expect(text).toEqual("ok");
+  const user = await User.findOne({ where: { id: userId } });
+  expect((user as User).confirmed).toBeTruthy();
+  const chunks = url.split("/");
+  const key = chunks[chunks.length - 1];
+  const value = await redis.get(key);
+  expect(value).toBeNull();
 });
